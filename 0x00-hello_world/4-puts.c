054e01ca43f9b7b6ef0ebf7f0c6f98b233989cb1
DustMite21 <thabo.crispen@gmail.com>
2023-03-16 20:35:43 +0000

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - This is called a function
+*Use the function puts
+*put - is a function used to output/print text to the screen
+*return : 0 means success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main : Indicates the main function
+  *return: success compile returns 0
+  *printf : Shows output or results
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
